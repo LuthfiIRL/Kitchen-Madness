74e6bee511e1571cda78e607fc698ff826c561b4
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-07 19:19:09 +0700

    Finisihing basic movement
    adding physical movement on k and y axis, adding time.deltatime, and adding moveSpeed variable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField] private float moveSpeed = 7f;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0,0);
@@ -26,6 +28,9 @@ public class Player : MonoBehaviour
 
         inputVector = inputVector.normalized;
 
-        Debug.Log(inputVector);       
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        Debug.Log(Time.deltaTime);       
     }
 }
