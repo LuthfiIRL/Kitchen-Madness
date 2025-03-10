cc2ffcb1c810b6d4d5f569621be05cee2d9e26c5
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-10 19:13:25 +0700

    Finishing Walk Animation
    also additionally installing Cinemachine

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string IS_WALKING = "IsWalking";
+    [SerializeField] private Player player;
+    private Animator animator;
+
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        animator.SetBool(IS_WALKING, player.IsWalking());        
     }
 }
