17ae576137d9644bf47f838829bb4edcb920d9ed
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-16 18:58:21 +0700

    Counter Visual
    changing counter visual when player can interact, by adding different material to a duplicate of the visual, and disbale/enable the visual using logic. part 1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
 
     private void Start()
     {
@@ -17,22 +18,10 @@ public class Player : MonoBehaviour
     }
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero)
+        if (selectedCounter != null)
         {
-            lastInteractDir = moveDir;            
-        }
-        float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
-        {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
-            {
-                //has ClearCounter component
-                clearCounter.Interact();
-            }            
-        } 
+            selectedCounter.Interact();
+        }         
     }
     private void Update()
     {        
@@ -60,9 +49,22 @@ public class Player : MonoBehaviour
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 //has ClearCounter component
+                if (clearCounter != selectedCounter)
+                {
+                    selectedCounter = clearCounter;
+                }               
                 
-            }            
-        }        
+            }    
+            else
+            {
+                selectedCounter = null;
+            }        
+        }
+        else
+        {
+            selectedCounter = null;
+        }    
+        Debug.Log(selectedCounter); 
     }
 
     private void HandleMovement()
