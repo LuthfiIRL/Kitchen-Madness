973d862cdfab08f8217f4f02dc361974014a174b
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-16 18:44:24 +0700

    Handling Interaction using Events

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using System;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
     private PlayerInputActions playerInputActions;
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        playerInputActions.Player.Interact.performed += Interact_performed;
+    }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);      
     }
     public Vector2 GetMovementVectorNormalized()
     {
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,30 @@ public class Player : MonoBehaviour
     [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
     private Vector3 lastInteractDir;
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;            
+        }
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //has ClearCounter component
+                clearCounter.Interact();
+            }            
+        } 
+    }
     private void Update()
     {        
         HandleMovement();  
@@ -36,7 +60,7 @@ public class Player : MonoBehaviour
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
                 //has ClearCounter component
-                clearCounter.Interact();
+                
             }            
         }        
     }
