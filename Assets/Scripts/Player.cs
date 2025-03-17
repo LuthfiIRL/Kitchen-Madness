87ce767c565c570ff8f3c948d02a31d0735666bb
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-17 19:11:11 +0700

    Using SIngletton pattern to change visual
    the counter visual now changed using events and singleton pattern

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs
     {
@@ -17,6 +18,15 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private Vector3 lastInteractDir;
     private ClearCounter selectedCounter;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than 1 player instance");
+        }
+        Instance = this;
+    }
 
     private void Start()
     {
