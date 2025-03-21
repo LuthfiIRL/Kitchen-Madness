9b5e8b117c24939c4aaa92c9a9c4097d69ee992b
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-21 19:11:35 +0700

    Adding ContainerCounter using Prefab Variant
    also making a base counter ckass for other counter to inherit

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounter : MonoBehaviour, IKitchenObjectParent
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;       
+
+    private KitchenObject kitchenObject;
+    public void Interact(Player player)
+    {
+        if (kitchenObject == null)
+        {
+            //spawn the kitchen object
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint); 
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);                
+        }
+        else
+        {
+            //give th eobject to the player
+            kitchenObject.SetKitchenObjectParent(player);            
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
