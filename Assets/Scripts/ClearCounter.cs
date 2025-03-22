3e0ccd348bb5fcb5f245daa9383a0a054280e9c5
LuthfiIRL <luthfirlangga22@gmail.com>
2025-03-22 19:14:03 +0700

    Adding ContainerCounter Pt.2
    refactoring the ClearCounter and ContainerCounter class to extends BaseCounter, fixing selectedVisual error on ContainerCounter, adding animation to ContainerCounter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -3,52 +3,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
 
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;       
-
-    private KitchenObject kitchenObject;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;    
     
 
-    public void Interact(Player player)
+    public override void Interact(Player player)
     {
-        if (kitchenObject == null)
-        {
-            //spawn the kitchen object
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint); 
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);                
-        }
-        else
-        {
-            //give th eobject to the player
-            kitchenObject.SetKitchenObjectParent(player);            
-        }
+        
     }
-
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return kitchenObject != null;
-    }
+    
 }
