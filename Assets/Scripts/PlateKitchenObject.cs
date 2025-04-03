5792601ff963853d60b79073a1d4365579486e0b
LuthfiIRL <luthfirlangga22@gmail.com>
2025-04-03 10:22:56 +0700

    KitcheeObject on Plate
    - Preventing a full kitchen object (ex: Full Tomato) to be picked up with plate by adding valid KitchenObjectSO list in PlateKitchenObject.cs
    
    - Make all the Plate pick up KitchenObject logic work in other counters (not finished)

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    [SerializeField] private List<KitchenObjectSO> valikdKitchenObjectSOList;
     private List<KitchenObjectSO> kitchenObjectSOList;
 
     private void Awake()
@@ -12,6 +13,11 @@ public class PlateKitchenObject : KitchenObject
     }
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
+        if (!valikdKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            // Not a valid kitchen object
+            return false;
+        }
         if (kitchenObjectSOList.Contains(kitchenObjectSO))
         {
             // Already has this type
