1bc26ef280f2f80f232e4916ee8a04552886ee38
LuthfiIRL <luthfirlangga22@gmail.com>
2025-04-15 17:48:36 +0700

    Adding Sound Effect Pt. 3
    - Adding reference of AudioClipRefsSO to SoundManager.cs and add new function for AudioClip[] there
    - Adding sound effect on delivery succes/fail
    - Adjusting the volume on delivery sound by creating a singleton in DeliveryCOunter.cs to access its position in SoundManager.cs so the sound can be played in DeliverySounter

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
     private void Start()
     {
         DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
@@ -12,12 +13,19 @@ public class SoundManager : MonoBehaviour
 
     private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e)
     {
-        // PlaySound()
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipRefsSO.deliverySuccess, deliveryCounter.transform.position);
     }
 
     private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e)
     {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipRefsSO.deliveryFail, deliveryCounter.transform.position);        
+    }
 
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    {
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);        
     }
 
     private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
